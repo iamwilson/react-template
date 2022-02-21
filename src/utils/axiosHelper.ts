ca396a549c99ccc44bdae36757d6998936399385
iamwilson <irewilson@gmail.com>
2022-02-21 23:00:07 +0800

    :zap: catch error in axios helper

--- src/utils/axiosHelper.ts
+++ src/utils/axiosHelper.ts
@@ -12,9 +12,21 @@ export const client = () => {
 	};
 
 	return {
-		get: (url: string, options = {}) => axios.get(url, { ...defaultOptions, ...options }).catch((error) => { return error?.response; }),
-		post: (url: string, data: any, options = {}) => axios.post(url, data, { ...defaultOptions, ...options }).catch((error) => { return error?.response; }),
-		put: (url: string, data: any, options = {}) => axios.put(url, data, { ...defaultOptions, ...options }).catch((error) => { return error?.response; }),
-		delete: (url: string, options = {}) => axios.delete(url, { ...defaultOptions, ...options }).catch((error) => { return error?.response; }),
+		get: (url: string, options = {}) =>
+			axios.get(url, { ...defaultOptions, ...options }).catch((error) => {
+				return error?.response;
+			}),
+		post: (url: string, data: any, options = {}) =>
+			axios.post(url, data, { ...defaultOptions, ...options }).catch((error) => {
+				return error?.response;
+			}),
+		put: (url: string, data: any, options = {}) =>
+			axios.put(url, data, { ...defaultOptions, ...options }).catch((error) => {
+				return error?.response;
+			}),
+		delete: (url: string, options = {}) =>
+			axios.delete(url, { ...defaultOptions, ...options }).catch((error) => {
+				return error?.response;
+			}),
 	};
 };
